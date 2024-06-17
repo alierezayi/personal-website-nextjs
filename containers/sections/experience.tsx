473e50305590a5c8c5b1a281d757a03a251c6d37
Ali Rezaei <im1.bitcode@gmail.com>
2024-06-17 22:35:15 +0330

    styling vertical timeline

--- containers/sections/experience.tsx
+++ containers/sections/experience.tsx
@@ -7,6 +7,7 @@ import {
   SectionSubTitle,
 } from "@/components/sections";
 import { EXPERIENCES } from "@/constants/refrences/experiences";
+import { useTheme } from "next-themes";
 import React from "react";
 import {
   VerticalTimeline,
@@ -15,25 +16,58 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 export default function Experience() {
+  const { theme } = useTheme();
   return (
     <Section name="Experience">
       <SectionHeading>Experience</SectionHeading>
-      <SectionSubTitle>My personal qualification</SectionSubTitle>
+      <SectionSubTitle className="">My personal qualification</SectionSubTitle>
       <SectionContent>
-        <VerticalTimeline>
+        <VerticalTimeline lineColor="#e5e7eb3e">
           {EXPERIENCES.map((experience, i) => (
             <React.Fragment key={i}>
               {experience.title ? (
                 <VerticalTimelineElement
+                  contentStyle={{
+                    background: theme === "dark" ? "#020617" : "#fffff",
+                    border:
+                      theme === "dark"
+                        ? "1px solid #374151"
+                        : "1px solid #e5e7eb",
+                    textAlign: "left",
+                    borderRadius: "16px",
+                    boxShadow: "none",
+                  }}
+                  contentArrowStyle={{
+                    borderRight:
+                      theme === "dark"
+                        ? "7px solid  #020617"
+                        : "7px solid  #fffff",
+                  }}
+                  iconStyle={{
+                    background: theme === "dark" ? "#020617" : "#fffff",
+                    fontSize: "1.25rem",
+                  }}
                   date={experience.date}
                   icon={experience.icon}
                 >
-                  <h3>{experience.title}</h3>
-                  <p>{experience.location}</p>
-                  <p>{experience.description}</p>
+                  <h3 className="font-semibold text-base">
+                    {experience.title}
+                  </h3>
+                  <p className="font-normal !mt-1 !text-xs">
+                    {experience.location}
+                  </p>
+                  <p className="!mt-5 !font-normal text-neutral-600 dark:text-neutral-200">
+                    {experience.description}
+                  </p>
                 </VerticalTimelineElement>
               ) : (
-                <VerticalTimelineElement icon={experience.icon} />
+                <VerticalTimelineElement
+                  iconStyle={{
+                    background: "#64748b",
+                    fontSize: "1.5rem",
+                  }}
+                  icon={experience.icon}
+                />
               )}
             </React.Fragment>
           ))}
